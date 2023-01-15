28fe6b5cd5ef3c47bb3e6aa4d191e2a6cf3d2a01
Vpolyushkevich@gmail.com <Vasily>
2023-01-15 12:15:45 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+System.Console.WriteLine("Введите первое число :");
+int numA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число :");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA > numB)
+{
+    System.Console.WriteLine($"Большее число : {numA}, Меньшее число : {numB}");
+}
+else
+{
+    System.Console.WriteLine($"Большее число : {numB}, Меньшее число : {numA}");
+}
+
